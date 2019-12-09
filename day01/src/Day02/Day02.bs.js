adb8fdd9b292eab0242719114f9c61cb90cf3c11
Fernando Montoya <montogeek@gmail.com>
2019-12-09 06:53:42 +0100

    Completed day02 part2

--- day01/src/Day02/Day02.bs.js
+++ day01/src/Day02/Day02.bs.js
@@ -2,6 +2,7 @@
 'use strict';
 
 var Fs = require("fs");
+var $$Array = require("bs-platform/lib/js/array.js");
 var Caml_array = require("bs-platform/lib/js/caml_array.js");
 var Caml_int32 = require("bs-platform/lib/js/caml_int32.js");
 var Caml_format = require("bs-platform/lib/js/caml_format.js");
@@ -41,13 +42,61 @@ var Part1 = {
   main: main
 };
 
+function processInput$1(input, index, noun, verb) {
+  var values = $$Array.of_list(input);
+  Caml_array.caml_array_set(values, 1, noun);
+  Caml_array.caml_array_set(values, 2, verb);
+  var result = values;
+  var operation = Caml_format.caml_int_of_string(Caml_array.caml_array_get(values, index + 0 | 0));
+  if (operation === 99) {
+    result = result;
+  } else {
+    var firstInput = Caml_format.caml_int_of_string(Caml_array.caml_array_get(values, Caml_format.caml_int_of_string(Caml_array.caml_array_get(values, index + 1 | 0))));
+    var secondInput = Caml_format.caml_int_of_string(Caml_array.caml_array_get(values, Caml_format.caml_int_of_string(Caml_array.caml_array_get(values, index + 2 | 0))));
+    var outputPosition = Caml_format.caml_int_of_string(Caml_array.caml_array_get(values, index + 3 | 0));
+    if (operation === 1) {
+      Caml_array.caml_array_set(result, outputPosition, String(firstInput + secondInput | 0));
+      result = processInput$1($$Array.to_list(result), index + 4 | 0, noun, verb);
+    }
+    if (operation === 2) {
+      Caml_array.caml_array_set(result, outputPosition, String(Caml_int32.imul(firstInput, secondInput)));
+      result = processInput$1($$Array.to_list(result), index + 4 | 0, noun, verb);
+    }
+    
+  }
+  return result;
+}
+
+function main$1(input) {
+  var values = input.split(",");
+  var result = /* record */[/* contents */values];
+  for(var i = 0; i <= 99; ++i){
+    for(var j = 0; j <= 99; ++j){
+      result[0] = processInput$1($$Array.to_list(values), 0, String(i), String(j));
+      if (Caml_array.caml_array_get(result[0], 0) === "19690720") {
+        console.log(Caml_int32.imul(100, i) + j | 0);
+      }
+      
+    }
+  }
+  return result;
+}
+
+var Part2 = {
+  processInput: processInput$1,
+  main: main$1
+};
+
 var Day02 = {
-  Part1: Part1
+  Part1: Part1,
+  Part2: Part2
 };
 
 var file = Fs.readFileSync("./input.txt", "utf8");
 
 console.log(main(file));
+
+console.log(main$1(file));
 
 exports.Day02 = Day02;
 exports.file = file;
--- day01/src/Day02/Day02.re
+++ day01/src/Day02/Day02.re
@@ -14,7 +14,7 @@ module Day02 = {
           int_of_string(values[int_of_string(values[index + 2])]);
         let outputPosition = int_of_string(values[index + 3]);
 
-        if (operation === 1) {  
+        if (operation === 1) {
           result^[outputPosition] = string_of_int(firstInput + secondInput);
           result := processInput(result^, index + 4);
         };
@@ -36,8 +36,60 @@ module Day02 = {
       result[0];
     };
   };
+
+  module Part2 = {
+    let rec processInput = (input, index, noun, verb) => {
+      let values = Array.of_list(input)
+      values[1] = noun;
+      values[2] = verb;
+      let result = ref(values);
+
+      let operation = int_of_string(values[index + 0]);
+
+      if (operation === 99) {
+        result := result^;
+      } else {
+        let firstInput =
+          int_of_string(values[int_of_string(values[index + 1])]);
+        let secondInput =
+          int_of_string(values[int_of_string(values[index + 2])]);
+        let outputPosition = int_of_string(values[index + 3]);
+
+        if (operation === 1) {
+          result^[outputPosition] = string_of_int(firstInput + secondInput);
+          result := processInput(Array.to_list(result^), index + 4, noun, verb);
+        };
+
+        if (operation === 2) {
+          result^[outputPosition] = string_of_int(firstInput * secondInput);
+          result := processInput(Array.to_list(result^), index + 4, noun, verb);
+        };
+      };
+
+      result^;
+    };
+
+    let main = input => {
+      let values = Js.String.split(",", input);
+      let result = ref(values);
+      let goal = "19690720";
+
+      for (i in 0 to 99) {
+        for (j in 0 to 99) {
+          result :=
+            processInput(Array.to_list(values), 0, string_of_int(i), string_of_int(j));
+
+          if (result^[0] === goal) {
+            Js.log(100 * i + j);
+          };
+        };
+      };
+      result;
+    };
+  };
 };
 
 let file = Node.Fs.readFileAsUtf8Sync("./input.txt");
 
-Day02.Part1.main(file)->Js.log;
+Day02.Part1.main(file)->Js.log
+Day02.Part2.main(file)->Js.log;
